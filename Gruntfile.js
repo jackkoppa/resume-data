83532680ebf9175b20c5b1cfaf0e5c1e21045617
Jack Koppa <hello@jackkoppa.com>
2017-02-17 14:05:43 -0600

    add jsonlint & method to remove comments

---
+++ Gruntfile.js
@@ -0,0 +1,29 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        stripJsonComments: {
+            dev: {
+                options:  {
+                    whitespace: false
+                },
+                files: {
+                    'resume.json' : 'resume_annotated.json'
+                }
+            },
+        },
+        jsonlint: {
+            dev: {
+                src: [ 'resume.json' ],
+                options: {
+                    formatter: 'prose'
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-strip-json-comments');
+    grunt.loadNpmTasks('grunt-jsonlint');
+
+    grunt.registerTask('default', ['stripJsonComments','jsonlint']);
+};
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# resume-data
-JSON source for current &amp; future projects requiring resume info - bio, previous projects, links, &amp; images
+## Summary
+JSON source for current &amp; future projects requiring resume info - bio, previous projects, links, &amp; images.
+
+## Usage
+Currently, the repo is intended to be cloned within the project drawing data from it, and accessed by a local AJAX call or with a server-side conversion into a JS variable. Image links are local to this repo.
